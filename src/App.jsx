4080c093d12aacfdb9eafb28bf5a556368ea99d0
Aleksandr Shalugin <ashalugin@optimajet.com>
2021-10-11 19:16:48 +0300

    Added DWKitStore usage example.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DWKitFormBuilder, DWKitFormViewer } from "./optimajet-builder";
+import { DWKitFormBuilder, DWKitFormViewer, DWKitStore } from "./optimajet-builder";
 import { builderProps, viewerProps } from "./builder-viewer-props";
 
 const HideIf = ({ condition, ...props }) => <div style={{ display: condition ? "none" : "initial" }} {...props} />
@@ -19,6 +19,11 @@ class App extends React.Component {
   componentDidMount() {
     this.builderRef?.current?.load('./application-form');
     this.setState({ dataurl: "./application-form-data.json" })
+    DWKitStore.listen(function (form) {
+      // log form data on change
+      console.log('Form data changed');
+      console.log(JSON.stringify(form));
+    });
   };
 
   toggleViewMode = () => {
@@ -40,7 +45,7 @@ class App extends React.Component {
         <div {...headerProps}> <button {...buttonProps} /> </div>
         {showViewer && <DWKitFormViewer {...{ ...viewerProps, model, dataurl }} />}
         <HideIf condition={showViewer}>
-          <DWKitFormBuilder {...builderProps} model={model} ref={this.builderRef} />
+          <DWKitFormBuilder {...builderProps} ref={this.builderRef} />
         </HideIf>
       </div>
     );
